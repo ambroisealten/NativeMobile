35eb4814dd367b0e58ce6bafe3c9de6e6a50e34c
mmaquinghen <maxime.maquinghen@alten.com>
2019-06-11 15:01:34 +0200

    modal v0.9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { ForumComponent } from "./forum/forum.component";
 
 const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
-    { path: "home", loadChildren: "~/app/home/home.module#HomeModule" }
+    { path: "home", loadChildren: "~/app/home/home.module#HomeModule" },
+    { path: "forum", component: ForumComponent},
+
 ];
 
 @NgModule({
